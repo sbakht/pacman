75c306dd8d238e05d5de9be0e5a79c60a485abd9
Saad Bakht <saad.bakht@bloomingdales.com>
2019-09-17 15:23:44 -0400

    can move player only onto open spaces and food

--- javascript/index.js
+++ javascript/index.js
@@ -3,13 +3,64 @@ const Piece = {
     Wall: "Wall",
     Food: "Food",
     Player: "Player",
+    OpenSpace: "OpenSpace",
 };
 
+// mkLocation: Int -> Int -> Location
 function mkLocation(x,y) {
     return {x, y};
 }
 
+const Direction = {
+  Up: 'Up',
+    Down: 'Down',
+    Left: 'Left',
+    Right: 'Right',
+};
+
+// getPiece: Location -> Grid -> Piece
+function getPiece({x, y}, grid) {
+  return grid[x][y];
+}
+
+function mvLocation({x, y}, direction) {
+  switch (direction) {
+    case Direction.Up:
+      return {x, y: y-1};
+    case Direction.Down:
+      return {x, y: y+1};
+    case Direction.Left:
+      return {x: x-1, y};
+    case Direction.Right:
+      return {x: x+1, y}
+  }
+}
+
+// isValidMove: Location -> Grid -> Bool
+function isValidMove(destination, grid) {
+  return getPiece(destination, grid) !== Piece.Wall
+}
+
+function mvPiece(source, destination, grid) {
+  grid[source.x][source.y] = Piece.OpenSpace;
+  grid[destination.x][destination.y] = Piece.Player;
+  return grid
+}
+
+const Actions = {
+  mvPlayerLeft: function (playerLocation, grid) {
+    const destination = mvLocation(playerLocation, Direction.Left);
+    return mvPiece(playerLocation, destination, grid)
+  }
+}
+
 module.exports = {
     Piece,
-    mkLocation
+    Direction,
+    mkLocation,
+    mvLocation,
+    getPiece,
+    isValidMove,
+    mvPiece,
+    Actions,
 }
--- javascript/index.test.js
+++ javascript/index.test.js
@@ -6,6 +6,7 @@ test('has game pieces', () => {
   expect(Piece.Wall).toBe("Wall");
   expect(Piece.Player).toBe("Player");
   expect(Piece.Food).toBe("Food");
+  expect(Piece.OpenSpace).toBe("OpenSpace");
 });
 
 test('has Location', () => {
@@ -13,6 +14,38 @@ test('has Location', () => {
     expect(loc).toEqual({x: 1, y: 2})
 })
 
-test('can go up', () => {
+test('can get new location from direction', () => {
+  const loc = index.mkLocation(0,0);
+  const mvLocation = index.mvLocation;
+  const Direction = index.Direction;
+  expect(mvLocation(loc, Direction.Up)).toEqual({x: 0, y: -1});
+  expect(mvLocation(loc, Direction.Down)).toEqual({x: 0, y: 1});
+  expect(mvLocation(loc, Direction.Left)).toEqual({x: -1, y: 0});
+  expect(mvLocation(loc, Direction.Right)).toEqual({x: 1, y: 0});
+});
 
+test('get piece from a grid', () => {
+  const getPiece = index.getPiece;
+  const grid = [[Piece.Wall, Piece.Food],[Piece.Wall, Piece.Player]];
+  const loc = index.mkLocation(0,0);
+  expect(getPiece(loc, grid)).toBe(Piece.Wall);
+})
+
+test('cant move into walls', () => {
+  const isValidMove = index.isValidMove;
+  const wallLoc = index.mkLocation(1,0);
+  const foodLoc = index.mkLocation(0,1);
+  const grid = [[Piece.Wall, Piece.Food],[Piece.Wall, Piece.Player]];
+  expect(isValidMove(wallLoc, grid)).toBe(false);
+  expect(isValidMove(foodLoc, grid)).toBe(true);
+});
+
+test('should move player', () => {
+  const Actions = index.Actions;
+  const playerLocation = index.mkLocation(1,1);
+
+  const grid = [[Piece.Wall, Piece.Food],[Piece.Wall, Piece.Player]];
+  const grid1 = Actions.mvPlayerLeft(playerLocation, grid);
+
+  expect(grid1).toEqual([[Piece.Wall, Piece.Player],[Piece.Wall, Piece.OpenSpace]]);
 });
